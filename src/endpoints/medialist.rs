b80c418f1c674b4ba1dbc6b28fdda8b397cdf793
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-10-03 01:28:45 +0530

    Updated Medialist Endpoint

--- src/endpoints/medialist.rs
+++ src/endpoints/medialist.rs
@@ -6,128 +6,100 @@ use crate::errors::AniListError;
 use crate::objects::responses::{UserMediaListResponse, SaveMediaListEntryResponse, DeleteMediaListEntryResponse};
 use serde::Serialize;
 use serde_json::json;
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Default, Serialize)]
 pub struct FetchMediaListOptions {
-    #[serde(skip_serializing_if = "Option::is_none", rename = "userId")]
+    #[serde(rename = "userId")]
     pub user_id: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "userName")]
+    #[serde(rename = "userName")]
     pub user_name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "type")]
+    #[serde(rename = "type")]
     pub media_type: Option<MediaType>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub status: Option<MediaListStatus>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "mediaId")]
+    #[serde(rename = "mediaId")]
     pub media_id: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "isFollowing")]
+    #[serde(rename = "isFollowing")]
     pub is_following: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub notes: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "startedAt")]
+    #[serde(rename = "startedAt")]
     pub started_at: Option<FuzzyDate>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "completedAt")]
+    #[serde(rename = "completedAt")]
     pub completed_at: Option<FuzzyDate>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "compareWithAuthList")]
+    #[serde(rename = "compareWithAuthList")]
     pub compare_with_auth_list: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "userId_in")]
+    #[serde(rename = "userId_in")]
     pub user_id_in: Option<Vec<i32>>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "status_in")]
+    #[serde(rename = "status_in")]
     pub status_in: Option<Vec<MediaListStatus>>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "status_not_in")]
+    #[serde(rename = "status_not_in")]
     pub status_not_in: Option<Vec<MediaListStatus>>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "status_not")]
+    #[serde(rename = "status_not")]
     pub status_not: Option<MediaListStatus>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "mediaId_in")]
+    #[serde(rename = "mediaId_in")]
     pub media_id_in: Option<Vec<i32>>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "mediaId_not_in")]
+    #[serde(rename = "mediaId_not_in")]
     pub media_id_not_in: Option<Vec<i32>>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "notes_like")]
+    #[serde(rename = "notes_like")]
     pub notes_like: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "startedAt_greater")]
+    #[serde(rename = "startedAt_greater")]
     pub started_at_greater: Option<FuzzyDate>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "startedAt_lesser")]
+    #[serde(rename = "startedAt_lesser")]
     pub started_at_lesser: Option<FuzzyDate>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "startedAt_like")]
+    #[serde(rename = "startedAt_like")]
     pub started_at_like: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "completedAt_greater")]
+    #[serde(rename = "completedAt_greater")]
     pub completed_at_greater: Option<FuzzyDate>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "completedAt_lesser")]
+    #[serde(rename = "completedAt_lesser")]
     pub completed_at_lesser: Option<FuzzyDate>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "completedAt_like")]
+    #[serde(rename = "completedAt_like")]
     pub completed_at_like: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub sort: Option<Vec<MediaListSort>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub page: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "perPage")]
+    #[serde(rename = "perPage")]
     pub per_page: Option<i32>,
 }
 
+#[skip_serializing_none]
 #[derive(Default, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct SaveMediaListOptions {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "mediaId")]
     pub media_id: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub status: Option<MediaListStatus>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub score: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "scoreRaw")]
     pub score_raw: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub progress: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "progressVolumes")]
     pub progress_volumes: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub repeat: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub priority: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub private: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub notes: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "hiddenFromStatusLists")]
     pub hidden_from_status_lists: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "customLists")]
     pub custom_lists: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "advancedScores")]
     pub advanced_scores: Option<Vec<f64>>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "startedAt")]
     pub started_at: Option<FuzzyDate>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "completedAt")]
     pub completed_at: Option<FuzzyDate>,
 }
 
+#[skip_serializing_none]
 #[derive(Default, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct SaveMediaListMultipleOptions {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub status: Option<MediaListStatus>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub score: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "scoreRaw")]
     pub score_raw: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub progress: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "progressVolumes")]
     pub progress_volumes: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub repeat: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub priority: Option<i32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub private: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub notes: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "hiddenFromStatusLists")]
     pub hidden_from_status_lists: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "advancedScores")]
     pub advanced_scores: Option<Vec<f64>>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "startedAt")]
     pub started_at: Option<FuzzyDate>,
-    #[serde(skip_serializing_if = "Option::is_none", rename = "completedAt")]
     pub completed_at: Option<FuzzyDate>,
-    #[serde(rename = "ids")]
     pub ids: Vec<i32>,
 }
 
@@ -150,8 +122,9 @@ impl MediaListEndpoint {
         options: FetchMediaListOptions,
     ) -> Result<UserMediaListResponse, AniListError> {
         let query = medialist::FETCH;
-        let variables = json!(options);
+        let variables = serde_json::to_value(options)?;
         let variables_map = crate::utils::json_to_hashmap(variables);
+        print!("{:#?}", variables_map);
         self.client.query_typed(query, Some(&variables_map)).await
     }
 
